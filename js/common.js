76df016d3a4f8e466b396d49800356452dcffd00
sswum <l5450won@naver.com>
2024-03-29 12:53:01 +0900

    자바스크립트 추가 올림

---
+++ js/common.js
@@ -0,0 +1,14 @@
+window.addEventListener("DOMContentLoaded",function() {
+
+
+
+const swiper = new Swiper(".mySwiper", {
+navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+         },
+         pagination: {
+            el: ".swiper-pagination",
+          },
+    });
+});
